061dcfc09c26ba304e0d43b60aac8dc6ff4114cc
skysys <gagan07pradhan@gmail.com>
2026-01-24 14:24:21 +0530

    Neuron Code Updated

---
+++ 5_BackPropogation/1.Neuron_BP.py
@@ -0,0 +1,81 @@
+# Forward Pass
+x = [1.0, -2.0, 3.0]
+w = [-3.0, -1.0, 2.0]
+b = 1.0
+
+# Multiplying inputs by weights
+xw0 = x[0]*w[0]
+xw1 = x[1]*w[1]
+xw2 = x[2]*w[2]
+
+# Weighted Sum of weighted inputs and bias
+z = xw0 + xw1 + xw2 + b
+
+# ReLU Activation Function
+y = max(0, z)
+print("The ouput is : ", y)
+
+# Backward Pass 
+
+# The derivative from the next layer 
+d_value = 1.0
+
+# Derivative of ReLU
+drelu_dz = d_value* (1.0 if z > 0 else 0.0)
+print(drelu_dz)
+
+# Partial Derivatives of Weighted Sum
+dsum_xw0 = 1
+dsum_xw1 = 1
+dsum_xw2 = 1
+dsum_b = 1
+drelu_dxw0 = drelu_dz * dsum_xw0
+drelu_dxw1 = drelu_dz * dsum_xw1
+drelu_dxw2 = drelu_dz * dsum_xw2
+drelu_db = drelu_dz * dsum_b
+print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
+
+# Partial Derivatives of the Multiplication function
+dmul_dx0 = w[0]
+dmul_dx1 = w[1]
+dmul_dx2 = w[2]
+dmul_dw0 = x[0]
+dmul_dw1 = x[1]
+dmul_dw2 = x[2]
+
+# Partial Derivative of the whole function w.r.t respective parameter
+drelu_dx0 = drelu_dz*dmul_dx0
+drelu_dx1 = drelu_dz*dmul_dx1
+drelu_dx2 = drelu_dz*dmul_dx2
+drelu_dw0 = drelu_dz*dmul_dw0
+drelu_dw1 = drelu_dz*dmul_dw1
+drelu_dw2 = drelu_dz*dmul_dw2
+print(drelu_dx0, drelu_dx1, drelu_dx2, drelu_dw0, drelu_dw1, drelu_dw2)
+
+dx = [drelu_dx0, drelu_dx1, drelu_dx2] # gradient on inputs
+dw = [drelu_dw0, drelu_dw1, drelu_dw2] # gradient on weights
+db = drelu_db # gradient on bias
+
+print(w, b)
+
+# Applying a fraction of gradients so as to decrease the output
+w[0] += -0.001 * dw[0]
+w[1] += -0.001 * dw[1]
+w[2] += -0.001 * dw[2]
+b += -0.001 * db
+
+#Forward Pass 
+xw0 = w[0] * x[0]
+xw1 = w[1] * x[1]
+xw2 = w[2] * x[2]
+
+z = xw0 + xw1 + xw2 + b
+
+y = max(0, z)
+# The final output of the chained ReLU function which has decreased
+print("The ouput is : ", y)
+
+
+
+
+
