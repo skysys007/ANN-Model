8ec52b2518f712e9ee32e208d3982b1e9b796504
skysys <gagan07pradhan@gmail.com>
2026-01-26 13:36:24 +0530

    CCEL & SM Function||docs

--- 5_BackPropogation/3.ReLU_CCEL_SoftMax_BP.py
+++ 5_BackPropogation/3.ReLU_CCEL_SoftMax_BP.py
@@ -34,6 +34,32 @@ class Activation_ReLU:
         self.dinputs = dvalues.copy()
         self.dinputs[dvalues<=0] = 0 
 
+#SoftMax Function
+class Activation_Softmax:
+    
+    #Forward Pass
+    def forward(self, inputs):
+        #Exponentiate Values
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        #Normalization
+        probabilities = exp_values/np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+    #Backward Pass
+    def backward(self, dvalues):
+        #Create an unitialized array
+        self.dinputs = np.empty_like(dvalues)
+
+        # Iterate for outputs and gradients
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calc Jacobian Matrix
+            # diagflat to convert it into a diagonal matrix
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            # Calcuate sample wise gradients and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+
+
 # Common Loss Class
 class Loss:
     def calculate(self, output, y):
@@ -86,3 +112,24 @@ class Loss_Categorical_Cross_Entropy(Loss):
         self.dinputs = - y_true/dvalues
         # Normalize Gradient
         self.dinputs /= samples
+
+class Activation_SoftMax_Loss_CategoricalCrossentropy():
+    def __init__(self):
+        self.activation = Activation_Softmax()
+        self.loss = Loss_Categorical_Cross_Entropy() 
+
+        # Forward Pass
+    def forward(self, inputs, y_true):
+            self.activation.forward(inputs)
+            self.output = self.activation.output
+            return self.loss.calculate(self.output, y_true)
+        
+    def backward(self, dvalues, y_true):
+            samples = len(dvalues)
+            if len(y_true.shape) == 2:
+                y_true = np.argmax(y_true, axis=1)
+
+            self.dinputs = dvalues.copy()
+            self.dinputs[range(samples), y_true] -= 1
+            self.dinputs = self.dinputs / samples
+
